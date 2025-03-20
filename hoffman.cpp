536a73b421ccee98d6f793fb807869b71d4894e3
Dushyantsharma25 <147974809+Dushyantsharma25@users.noreply.github.com>
2025-03-21 00:31:36 +0530

    Update hoffman.cpp

--- hoffman.cpp
+++ hoffman.cpp
@@ -1,94 +1,87 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<queue>
-#include<unordered_map>
+#include <bits/stdc++.h>
 using namespace std;
 
-class node {
-    public:
+class node{
+public:
     int data;
-    node* left;
-    node* right;
+    node *left;
+    node *right;
 
-    node() {
+    node()
+    {
         this->data = 0;
         this->left = NULL;
         this->right = NULL;
     }
 
-    node(int data) {
+    node(int data)
+    {
         this->data = data;
         this->left = NULL;
         this->right = NULL;
     }
 };
 
-struct compare {
-    bool operator()(node* l, node* r) {
-        return l->data > r->data;
+void hoffman_code(node *a, string &d){
+    if(a->left == NULL && a->right == NULL){
+        cout<<a->data<<" "<<d<<endl;
+        return;
     }
-};
-
-void generate_codes(node* root, string code, unordered_map<int, string>& huffman_codes) {
-    if (!root) return;
-
-    // If this is a leaf node, store the code
-    if (!root->left && !root->right) {
-        huffman_codes[root->data] = code;
+    if (a->left)
+    {
+        d += '0';
+        hoffman_code(a->left, d);
     }
-
-    generate_codes(root->left, code + "0", huffman_codes);
-    generate_codes(root->right, code + "1", huffman_codes);
+    d.pop_back();
+    if (a->right)
+    {
+        d += '1';
+        hoffman_code(a->right, d);
+    }
+    d.pop_back();
 }
 
-void hoffman_tree(vector<node*>& nodes) {
-    priority_queue<node*, vector<node*>, compare> pq(nodes.begin(), nodes.end());
+void hoffman_tree(vector<pair<int, node *>> &a){
+    int n = a.size();
+    sort(a.begin(), a.end());
+    while (1)
+    {
+        int y = a[0].first + a[1].first;
+        node *x = new node(y);
 
-    while (pq.size() > 1) {
-        node* left = pq.top();
-        pq.pop();
-        node* right = pq.top();
-        pq.pop();
-
-        node* parent = new node(left->data + right->data);
-        parent->left = left;
-        parent->right = right;
-
-        pq.push(parent);
+        x->left = a[0].second;
+        x->right = a[1].second;
+        a.push_back({y, x});
+        a.erase(a.begin());
+        a.erase(a.begin());
+        sort(a.begin(), a.end());
+        if (a.size() == 1)
+        {
+            return;
+        }
     }
-
-    nodes.clear();
-    nodes.push_back(pq.top());
 }
 
-int main() {
+int main(){
+
     int n;
-    cout << "Enter number of nodes \n";
+    cout << "Enter number of nodes to be insert.\n";
     cin >> n;
-    vector<node*> nodes;
-    for (int i = 0; i < n; i++) {
-        int data;
-        cout << "Enter data for node " << i + 1 << endl;
-        cin >> data;
-        nodes.push_back(new node(data));
+
+    vector<pair<int, node *>> a;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element\n";
+        int d;
+        cin >> d;
+        node *value = new node(d);
+        a.push_back({d, value});
     }
 
-    hoffman_tree(nodes);
-
-    // The root of the Huffman Tree will be the only remaining node
-    node* root = nodes[0];
-    cout << "Root of Huffman Tree: " << root->data << endl;
-
-    // Generate Huffman codes
-    unordered_map<int, string> huffman_codes;
-    generate_codes(root, "", huffman_codes);
-
-    // Print Huffman codes
-    cout << "Huffman Codes:\n";
-    for (auto& pair : huffman_codes) {
-        cout << pair.first << ": " << pair.second << endl;
-    }
+    hoffman_tree(a);
+    string s = "";
+    hoffman_code(a[0].second, s);
 
     return 0;
 }
